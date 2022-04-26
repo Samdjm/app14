8e750cd1992321c6d43cc6830b3604d8e62fd861
NewEra <sam.djm93@gmail.com>
2022-04-26 17:01:37 +0200

    First Commit: PremierComposant

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let utilisateur = { prenom: "Julien", nom: "Djemai" };
+
+  function PremierComposant() {
+    return <p>Notre premier compsant!</p>;
+  }
+
+  function ComposantBonjour({ user, isPermis }) {
+    return (
+      <div>
+        <span> {user.prenom} </span>
+        <span>{user.nom}</span>
+        <p>{isPermis ? "Vous pouvez conduire" : "Prendre le bus"}</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Bonjour, bienvenu sur react!</p>
+        <p>{utilisateur.prenom}</p>
+        {PremierComposant()}
+        <PremierComposant />
+
+        {/* {ComposantBonjour(utilisateur)} */}
+        <ComposantBonjour user={utilisateur} isPermis={true} />
       </header>
     </div>
   );
